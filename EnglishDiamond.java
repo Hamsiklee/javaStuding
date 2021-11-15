6d1456e95c3a82763378ed2a63851e8dfbf0cb11
Hamsiklee <93498665+Hamsiklee@users.noreply.github.com>
2021-11-16 00:00:10 +0900

    Add files via upload
    ddd

--- EnglishDiamond.java
+++ EnglishDiamond.java
@@ -4,22 +4,24 @@ public class EnglishDiamond {
 	public EnglishDiamond() {
 	
 	}
-
 	
 	
 	
-	public void Diamond() {
 		
-		for(int i=1; i<=5; i++) {   
-            for (int j=1; j<=5-i; j++) {
+	public void Diamond() {
+		char c =  'A';
+		
+		for(int i= 0; i <=5; i++) {   
+            for (int j=1 ; j<=5-i  ; j++) {
                 System.out.print(" ");
             }
-            for (int j=1; j<=i; j++) {
-                System.out.print("*"); 
+            for (int j=1; j<=2*i+1; j++) {
+            	
+            	for(int i3 = 0; i3 < 26; i3++) {
+            		System.out.print(c++);
+            	}
             }
-            for (int j=2; j<=i; j++) {
-                System.out.print("*"); 
-            }
+           
             System.out.println();
             
         }
