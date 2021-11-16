8b8019ef1627894723cd9ee7c84f66aafd5119f8
Hamsiklee <93498665+Hamsiklee@users.noreply.github.com>
2021-11-17 02:57:14 +0900

    Add files via upload
    I have to remove it..

--- CtManager.java
+++ CtManager.java
@@ -1,12 +1,15 @@
 package Fuck;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
+import javax.management.StringValueExp;
 
 public class CtManager {
 	Scanner sc = new Scanner(System.in);
-//	List
+
 	public CtManager() {
 
 	}
@@ -17,90 +20,131 @@ public class CtManager {
 			String id = DataInput("id=");
 			String pwd = DataInput("password=");
 			CtLog log = new CtLog();
-			
+
 			logResult = log.CtLogOk(id, pwd);
 		} while (!logResult);
-		
+
 		System.out.println("종목" + "\t" + "브랜드" + "\t" + "가격" + "\t" + "수량" + "\t" + "위치" + "\t" + "관리자");
-					CtArray.ctList();
-	
-					System.out.println();
+
+		System.out.println();
+//		lst.
+		CtArray ca = new CtArray();
+		List<CtVO> lst = ca.ctList();
+		do {
+			String pick = DataInput("1.목록보여주기  2.수정하기  3.항목추가  4.항목삭제  5.종료 >>>>>");
+			if (pick.equals("1")) {
+				System.out.println("종목" + "\t" + "브랜드" + "\t" + "가격" + "\t" + "수량" + "\t" + "위치" + "\t" + "관리자");
+				for(CtVO cv :lst) {
+					System.out.println(cv.tostring());
+				}
 				
-		
-	 do {
-		 
-		 String pick = DataInput("1.목록보여주기  2.수정하기  3.항목추가  4.항목삭제  5.종료 >>>>>");		
-		 if (pick.equals("1")) {
-			 System.out.println("종목" + "\t" + "브랜드" + "\t" + "가격" + "\t" + "수량" + "\t" + "위치" + "\t" + "관리자");	
-			 CtArray.ctList();
-		}else if(pick.equals("2")){
-			remove();
-		}else if(pick.equals("3")){
-				addList();
-		}else if(pick.equals("4")) {
+			} else if (pick.equals("2")) {
+				removeEa();
+				removePlace();
+				removePeople();
+			}
+			else if (pick.equals("3")) {
+				
+				String newName = DataInput("종목명>>");
+				
+				String newBrand = DataInput("브랜드 입력>>");
+				
+				String newMoney = DataInput("가격 입력>>");
+				
+				
+				String newEa = DataInput("수량 입력>>");
+				
+				String newPlace = DataInput("장소 입력>>");
+				
+				String newAdmin = DataInput("관리자명>>");
+				
+				
+				
+//				lst.add(0,);
+				
+				
+				
+				System.out.println("종목" + "\t" + "브랜드" + "\t" + "가격" + "\t" + "수량" + "\t" + "위치" + "\t" + "관리자");
+				
+				
+				
+			} else if (pick.equals("4")) {
 				deleteList();
-		}else{ endList();}
-			
-	 }while(true);
-		 
+				
+			} else {
+				endList();
+			}
+
+		} while (true);
+
 	}
+
+
+	public void removeEa() {
 		
-	public void remove() {
-		String what = DataInput("수정할 브랜드명");
-		
-		removeEa(what);
-		removePlace(what);
-		removePeople(what);
 		
 	}
-	
-	
-	
-	public void removeEa(String brand) {
-		String rEa = DataInput("수정할 수량 입력");
-//		CtVO vo = new CtVO();
-//		vo.setEa(rEa);
+
+	public void removePlace() {
 		
-	
 	}
-	public void removePlace(String brand) {
-		String rPlace = DataInput("수정할 위치 입력");
-		CtVO vo = new CtVO();
-		vo.setPlace(rPlace);
+
+	public void removePeople() {
+
 	}
-	public void removePeople(String brand) {
-		String rPeople = DataInput("수정할 관리자 입력");
-		CtVO vo = new CtVO();
-		vo.setAdmin(rPeople);
-		CtArray.ctList();
-	}
-	
-		
-			
+
 //		do {
 //			
 //		}while(remove1.equals(list.))
-		
-		
-	
-	public void listList() {
 
-	}
-	
+//	public stvoid addList() {
+////
+////		String addList1 = DataInput("무슨 상품추가?(ex:키보드,키크론,3000,장소,관리자)");
+////		for (list ls : addList1) {
+////
+////		}
+//		
+//		CtVO cv = new CtVO();
+//		String newName = DataInput("종목명>>");
+//		
+//		String newBrand = DataInput("브랜드 입력>>");
+//		
+//		String newMoney = DataInput("가격 입력>>");
+//		
+//		
+//		String newEa = DataInput("수량 입력>>");
+//		
+//		String newPlace = DataInput("장소 입력>>");
+//		
+//		String newAdmin = DataInput("관리자명>>");
+//		
+//		CtArray ca = new CtArray();
+//		List<CtVO> lst = ca.ctList();
+//		CtVO cVO[] = new CtVO[lst.size()];
+//		lst.toArray(cVO);
+//		
+//		for(CtVO aVO : lst) {
+//			System.out.println(aVO);
+//		}
+		
+		
+//		for( CtVO aVo : cVO) {
+//			aVo.title();
+//		}
+////		
+//		System.out.println("종목" + "\t" + "브랜드" + "\t" + "가격" + "\t" + "수량" + "\t" + "위치" + "\t" + "관리자");
+//		
+//		
+//		}
+//		
+//	
 
-	public void addList() {
-		
-		String addList1 =DataInput("무슨 상품추가?(ex:키보드,마우스)");
-		String addList2 =DataInput("브랜드 이름? ");
-		String addList3 =DataInput("추가할 이름");
-		
-		
-	}
 	public void deleteList() {
 		
 	}
+
 	public void endList() {
-		
+
 	}
 
 	public String DataInput(String msg) {
