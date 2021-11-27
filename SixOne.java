5c76110782f2531a9793a8f1e57bb32224d9966e
Hamsiklee <93498665+Hamsiklee@users.noreply.github.com>
2021-11-27 23:24:24 +0900

    Add files via upload

---
+++ SixOne.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class SixOne {
+	//자료형은 double이고 요소 수가 5인 배열을 생성하고, 이 배열의 모든 요소를 표시하는 프로그램을 작성하여라 
+	public SixOne() {
+	}
+	public void start() {
+		double[] db = new double[5];
+		
+		System.out.println("db["+0+"] = " + db[0]);
+		System.out.println("db["+1+"] = " + db[1]);
+		System.out.println("db["+2+"] = " + db[2]);
+		System.out.println("db["+3+"] = " + db[3]);
+		System.out.println("db["+4+"] = " + db[4]);
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		new SixOne().start();
+		
+		
+	}
+
+}
